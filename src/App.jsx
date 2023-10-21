b2a303c4f4ba7dccde8b8709da4103b88e8786fb
thachthaiminh <gukucfvn@gmail.com>
2023-10-22 05:13:59 +0700

    Add Dark-Light-P1.

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,29 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import Typography from "@mui/material/Typography";
+import { useColorScheme } from "@mui/material/styles";
+import { Button } from "@mui/material";
+
+function ModeToggle() {
+  const { mode, setMode } = useColorScheme();
+  return (
+    <Button
+      onClick={() => {
+        setMode(mode === "light" ? "dark" : "light");
+      }}
+    >
+      {mode === "light" ? "Turn dark" : "Turn light"}
+    </Button>
+  );
+}
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
+      <ModeToggle />
+      <hr />
       <Typography variant="body1" color="text.secondary">
         thachthaiminh
       </Typography>
