114875f06ba69c6ec4a12564e3f66fba588e0872
thachthaiminh <gukucfvn@gmail.com>
2023-10-22 15:30:32 +0700

    Add DarkModeSelect.

--- src/App.jsx
+++ src/App.jsx
@@ -4,31 +4,70 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 import Typography from "@mui/material/Typography";
 import { useColorScheme } from "@mui/material/styles";
-import { Button } from "@mui/material";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+import LightModeIcon from "@mui/icons-material/LightMode";
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import SettingsBrightnessIcon from "@mui/icons-material/SettingsBrightness";
+import Box from "@mui/material/Box";
 
-function ModeToggle() {
+function ModeSelect() {
   const { mode, setMode } = useColorScheme();
+  const handleChange = (event) => {
+    const selectedMode = event.target.value;
+    setMode(selectedMode);
+  };
   return (
-    <Button
-      onClick={() => {
-        setMode(mode === "light" ? "dark" : "light");
-      }}
-    >
-      {mode === "light" ? "Turn dark" : "Turn light"}
-    </Button>
+    <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+      <InputLabel id="label-select-dark-light-mode">Mode</InputLabel>
+      <Select
+        labelId="label-select-dark-light-mode"
+        id="select-dark-light-mode"
+        value={mode}
+        label="Mode"
+        onChange={handleChange}
+      >
+        <MenuItem value="light">
+          <div style={{ display: "flex", alignItems: "center", gap: 1 }}>
+            <LightModeIcon fontSize="small" />
+            Light
+          </div>
+        </MenuItem>
+        <MenuItem value="dark">
+          <Box sx={{ display: "flex", alignItems: "center", gap: 1 }}>
+            <DarkModeOutlinedIcon fontSize="small" />
+            Dark
+          </Box>
+        </MenuItem>
+        <MenuItem value="system">
+          <Box sx={{ display: "flex", alignItems: "center", gap: 1 }}>
+            <SettingsBrightnessIcon fontSize="small" />
+            System
+          </Box>
+        </MenuItem>
+      </Select>
+    </FormControl>
   );
 }
+// function ModeToggle() {
+//   const { mode, setMode } = useColorScheme();
+//   return (
+//     <Button
+//       onClick={() => {
+//         setMode(mode === "light" ? "dark" : "light");
+//       }}
+//     >
+//       {mode === "light" ? "Turn dark" : "Turn light"}
+//     </Button>
+//   );
+// }
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <ModeToggle />
-      <hr />
-      <Typography variant="body1" color="text.secondary">
-        thachthaiminh
-      </Typography>
+      <ModeSelect />
     </>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  important: "#root",
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  corePlugins: {
+    preflight: false,
+  },
+};
